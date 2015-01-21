857fb71c2789aa3065229e67babe1837b9dae4e4
srblum <seanblum@gmail.com>
2015-01-20 18:02:03 -0800

    Added form for starting cash and bet amount, and added console log for results of 1000 simulations.

--- Vegas.js
+++ Vegas.js
@@ -1,13 +1,23 @@
-function simRoulette(startCash,betCash,numPlays){
+//Simulates a single night of roulette
+//Returns an array of 101 cash values
+function simRoulette(startCash,betCash){
 	var cashArr=[startCash];
+	var curCash=startCash;
 	var winFrac=18/37;
-	for(var i=0;i<numPlays;i++){
-		if(Math.random()<winFrac){
-			newCash+=betCash;
-		}else{
-			newCash-=betCash;
-		}
-		cashArr[cashArr.length]=newCash;
+	for(var i=0;i<100;i++){
+		if(curCash>0){
+			if(Math.random()<winFrac){
+				curCash+=betCash;
+			}else{curCash-=betCash;}
+		}else{}
+		cashArr[cashArr.length]=curCash;
 	}
 	return cashArr
+}
+
+
+//Takes a nested array of 1000 arrays 
+//of 101 cash values and plots them
+function drawPlot(cashArrs){
+	console.log('drawPlot was called')
 }
