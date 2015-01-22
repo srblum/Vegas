5aa2825b8e18625c8d77f7f9802eb965b472f13e
srblum <seanblum@gmail.com>
2015-01-22 14:45:15 -0800

    Randomized transition durations

--- Vegas.js
+++ Vegas.js
@@ -1,13 +1,13 @@
 //The main function of Vegas.js
 //Called when "start" button is clicked
-//Calls all other functions
 function runSim(form) {
     var startCash = Number(form.startCash.value);
     var betCash = Number(form.betCash.value);
-    var numNights = 200;
+    var numPlays = 100;
+    var numNights = 300;
     var cashArrs=[];
     for(var i=0;i<numNights;i++){
-		cashArrs[cashArrs.length]=simRoulette(startCash,betCash);
+		cashArrs[cashArrs.length]=simRoulette(startCash,betCash,numPlays);
     }
 
     //Store an average of all arrays of cashArr in aveArr
@@ -20,82 +20,69 @@ function runSim(form) {
 		ave/=cashArrs.length;
 		aveArr[aveArr.length]=ave;
 	}
-	//Specify the width and height of the svg element
-	var w = 900,
-	    h = 300;
-	    padding = 30;
+
+	//Specify the width, height, and margin of the svg element
+	var w = 600,
+	    h = 200;
+	    padding = 50;
 
 	//maps the domain of the data (0,length-1)
 	//onto the range of x screen coordinates (which correspond
 	//to the width of the svg element.)
 	var xScale = d3.scale.linear()
-	    .domain([0, 100])
+	    .domain([0, numPlays])
 	    .range([padding, w - padding * 2]);
 
-	//Same as above, but mapping y-data
-	//to y screen coordinates
 	var yScale = d3.scale.linear()
-	    .domain([0, 200])
+	    .domain([0, d3.max(cashArrs,function(x){return d3.max(x)})])
 	    .range([h - padding, padding]);
 
-	//d3.svg.line is a path generator
-	//It is both an object and a function
-	//Creates a line object with anonymous functions
-	//that tell the object which numbers in the data array 
-	//correspond to x- and y-values.
-	//
-	//Also uses the x and y d3 scale objects created above
+	//d3.svg.line is a path generator (both object and function), containing scale information
 	var line = d3.svg.line()
 	    .x(function(d) { return xScale(d[0]); })
 	    .y(function(d) { return yScale(d[1]); });
 
-	//appends an svg element to the body
-	//with apropriate width and height
+	//Recreate svg element
 	d3.select('body').selectAll('svg').remove();
 	var svg = d3.select('body').append('svg')
 	    .attr('width', w)
 	    .attr('height', h);
 
-	//Define X axis
+	//Define X and Y axis
 	var xAxis = d3.svg.axis()
 					  .scale(xScale)
 					  .orient("bottom")
 					  .ticks(1);
-
-	//Define Y axis
 	var yAxis = d3.svg.axis()
 					  .scale(yScale)
 					  .orient("left")
 					  .ticks(3);
 
-	//Create X axis
+	//Create X and Y axis
 	svg.append("g")
 		.attr("class", "axis")
 		.attr("transform", "translate(0," + (h - padding) + ")")
 		.call(xAxis);
-	
-	//Create Y axis
 	svg.append("g")
 		.attr("class", "axis")
 		.attr("transform", "translate(" + padding + ",0)")
 		.call(yAxis);
 
-	//Draw all 1000 simulations
+	//Draw all 1000 simulations and average
 	for(var i=0;i<cashArrs.length;i++){
 		drawPlot(cashArrs[i],svg,line);
 	}
-
-	//Draw average
 	drawAve(aveArr,svg,line);
 }
 
+
 //Simulates a single night of roulette (100 spins)
 //Returns an array of 101 cash values
-function simRoulette(startCash,betCash){
+function simRoulette(startCash,betCash,numPlays){
 	var cashArr=[startCash];
 	var curCash=startCash;
 	var winFrac=18/37;
-	for(var i=0;i<100;i++){
+	for(var i=0;i<numPlays;i++){
 		if(curCash>0){
 			if(Math.random()<winFrac){
 				curCash+=betCash;
@@ -106,31 +93,19 @@ function simRoulette(startCash,betCash){
 	return cashArr
 }
 
-//Takes an array of cash values and plots them
-//This code was taken from here:
+//Takes an array of cash values and plots them. Taken from here:
 //http://big-elephants.com/2014-06/unrolling-line-charts-d3js/
+//Documentation about transitions here: https://github.com/mbostock/d3/wiki/Transitions
 function drawPlot(cashArr,svg,line){
-	//Turn the raw data into a nested array of
-	//[index,data] pairs
-	//This works because index is an automatic
-	//2nd argument to the map function
 	var data = cashArr.map(function(d,i) {
         return [i, d];
     });
-    
-	//Appends a path element to the svg element created above
-    // add element and transition in
+    //Seemingly unnecessary line omitted below: .attr('d', line(data[0]))
     var path = svg.append('path')
         .attr('class', 'line')
-        //Seemingly unnecessary line omitted below
-        // .attr('d', line(data[0]))
-
-        //Documentation about transitions here:
-        //https://github.com/mbostock/d3/wiki/Transitions
         .transition()
-        .duration(1500)
+        .duration((Math.random()+2)*1000)
         .attrTween('d', pathTween);
-    //Called by attrTween during the path transition animation
 	function pathTween() {
 	    var interpolate = d3.scale.quantile()
 	            .domain([0,1])
@@ -150,9 +125,8 @@ function drawAve(aveArr,svg,line){
     var path = svg.append('path')
         .attr('class', 'ave')
         .transition()
-        .duration(1500)
+        .duration(2900)
         .attrTween('d', pathTween);
-    //Called by attrTween during the path transition animation
 	function pathTween() {
 	    var interpolate = d3.scale.quantile()
 	            .domain([0,1])
